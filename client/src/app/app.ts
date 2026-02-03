bdf674d53fc4442ea66016f07e990957d7c9140e
affaled <affaledl@gmail.com>
2026-02-03 02:34:53 -0300

    chamada do editor para o app

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Editor } from './features/editor/editor';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [Editor],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('flint-web');
-}
+export class App {}
